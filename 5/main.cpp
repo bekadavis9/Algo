6fec8ca42d727b05a2ccad0592c5a8892dc43b42
Beka <bekadavis9@gmail.com>
2018-12-04 15:33:12 -0500

    start project 5

---
+++ 5/main.cpp
@@ -0,0 +1,35 @@
+int main()
+{
+   char x;
+   ifstream fin;
+
+   // Read the maze from the file.
+   string fileName = "maze.txt";
+
+   fin.open(fileName.c_str());
+   if (!fin)
+   {
+      cerr << "Cannot open " << fileName << endl;
+      exit(1);
+   }
+
+   try
+   {
+
+      graph g;
+      while (fin && fin.peek() != 'Z')
+      {
+         maze m(fin);
+      }
+
+
+   }
+   catch (indexRangeError &ex)
+   {
+      cout << ex.what() << endl; exit(1);
+   }
+   catch (rangeError &ex)
+   {
+      cout << ex.what() << endl; exit(1);
+   }
+}
